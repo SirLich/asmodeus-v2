3c60da03b8d264f678f36dd665f09298994c0402
HowToZombie <zombie1productions@gmail.com>
2017-09-07 15:59:04 -0700

    Action Bar Util

---
+++ src/me/sirlich/AsmodeusRpg/utilities/ActionBarUtil.java
@@ -0,0 +1,17 @@
+package me.sirlich.AsmodeusRpg.utilities;
+
+import net.minecraft.server.v1_12_R1.ChatMessageType;
+import net.minecraft.server.v1_12_R1.IChatBaseComponent;
+import net.minecraft.server.v1_12_R1.PacketPlayOutChat;
+import org.bukkit.ChatColor;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class ActionBarUtil {
+    public static void sendActionBar(Player p, String msg) {
+        String s = ChatColor.translateAlternateColorCodes('&', msg);
+        IChatBaseComponent icbc = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + s + "\"}");
+        PacketPlayOutChat bar = new PacketPlayOutChat(icbc, ChatMessageType.GAME_INFO);
+        ((CraftPlayer) p).getHandle().playerConnection.sendPacket(bar);
+    }
+}
