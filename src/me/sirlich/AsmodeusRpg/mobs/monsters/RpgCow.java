d21c81aad41691a23e54fee773466d31e4f47fef
SirLich <lrkoehler77@gmail.com>
2018-01-22 01:10:07 -0600

    Remove all evidence of Attributes

--- src/me/sirlich/AsmodeusRpg/mobs/monsters/RpgCow.java
+++ src/me/sirlich/AsmodeusRpg/mobs/monsters/RpgCow.java
@@ -1,8 +1,6 @@
 package me.sirlich.AsmodeusRpg.mobs.monsters;
 
 import me.sirlich.AsmodeusRpg.mobs.pathfinders.PathFinderGoalReduceAggression;
-import me.sirlich.AsmodeusRpg.mobs.pathfinders.RpgAttributes;
-import me.sirlich.AsmodeusRpg.utilities.NMSUtils;
 import net.minecraft.server.v1_12_R1.*;
 import org.bukkit.craftbukkit.v1_12_R1.CraftServer;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftCow;
@@ -27,12 +25,6 @@ public class RpgCow extends EntityCow
         this.goalSelector.a(7, new PathfinderGoalRandomStrollLand(this, 1.0D));
         this.goalSelector.a(8, new PathfinderGoalLookAtPlayer(this, EntityHuman.class, 8.0F));
         this.goalSelector.a(8, new PathfinderGoalRandomLookaround(this));
-    }
-
-    @Override
-    protected void initAttributes() {
-        super.initAttributes();
-        this.getAttributeInstance(RpgAttributes.level).setValue(level);
     }
 
     private static class CraftRpgCow extends CraftCow
