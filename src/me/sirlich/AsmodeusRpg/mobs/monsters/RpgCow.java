c659caedd017983753ce961f206de0df42320285
SirLich <lrkoehler77@gmail.com>
2018-01-21 22:53:47 -0600

    Add Attribute system onto RpgCow
    This is the first guinea pig for the new system. Hopes are not high.

--- src/me/sirlich/AsmodeusRpg/mobs/monsters/RpgCow.java
+++ src/me/sirlich/AsmodeusRpg/mobs/monsters/RpgCow.java
@@ -1,21 +1,20 @@
 package me.sirlich.AsmodeusRpg.mobs.monsters;
 
 import me.sirlich.AsmodeusRpg.mobs.pathfinders.PathFinderGoalReduceAggression;
+import me.sirlich.AsmodeusRpg.mobs.pathfinders.RpgAttributes;
+import me.sirlich.AsmodeusRpg.utilities.NMSUtils;
 import net.minecraft.server.v1_12_R1.*;
 import org.bukkit.craftbukkit.v1_12_R1.CraftServer;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftCow;
 
-public class RpgCow extends EntityZombie
+public class RpgCow extends EntityCow
 {
-    private double damage;
-    private double knockback;
-
-    public RpgCow(World world, double damage, double knockback)
+    private double level;
+    public RpgCow(World world, Integer level)
     {
         super(world);
-        this.damage = damage;
-        this.knockback = knockback;
-        //this.bukkitEntity = new CraftRpgCow(this.world.getServer(), this);
+        this.level = (double) level;
+        this.bukkitEntity = new CraftRpgCow(this.world.getServer(), this);
     }
 
     @Override
@@ -28,6 +27,12 @@ public class RpgCow extends EntityZombie
         this.goalSelector.a(7, new PathfinderGoalRandomStrollLand(this, 1.0D));
         this.goalSelector.a(8, new PathfinderGoalLookAtPlayer(this, EntityHuman.class, 8.0F));
         this.goalSelector.a(8, new PathfinderGoalRandomLookaround(this));
+    }
+
+    @Override
+    protected void initAttributes() {
+        super.initAttributes();
+        this.getAttributeInstance(RpgAttributes.level).setValue(level);
     }
 
     private static class CraftRpgCow extends CraftCow
