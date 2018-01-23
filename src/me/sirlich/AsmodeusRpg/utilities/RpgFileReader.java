9337384b33ebd8cd8fa4c6746714405c4130000b
SirLich <lrkoehler77@gmail.com>
2018-01-22 20:52:18 -0600

    Add string support to RpgFileReader
    Support includes both level-based grabbing as well as random

--- src/me/sirlich/AsmodeusRpg/utilities/RpgFileReader.java
+++ src/me/sirlich/AsmodeusRpg/utilities/RpgFileReader.java
@@ -2,6 +2,7 @@ package me.sirlich.AsmodeusRpg.utilities;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Stream;
 
 public class RpgFileReader
@@ -57,6 +58,59 @@ public class RpgFileReader
                         //Generate the numbers
                         int[] ints = Stream.of(intList.split(",")).mapToInt (Integer::parseInt).toArray();
                         returnThis = ints[level];
+                    }
+                }
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return returnThis;
+    }
+
+    public String readString(String tag, int level){
+        String returnThis = "NULL PLEASE FIX";
+        if(level >= 10){
+            //Early cancelation because the level is wacked out.
+            return returnThis;
+        }
+        try {
+            String line = reader.readLine();
+            while (line != null)
+            {
+                if(line.charAt(0) == '['){
+                    //Is it our line?
+                    if(line.contains(tag)){
+                        //Take the second half
+                        String stringList = line.split("]")[1];
+                        String[] strings = stringList.split(",");
+                        returnThis = strings[level].trim();
+                    }
+                }
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return returnThis;
+    }
+
+    public String readString(String tag){
+        String returnThis = "NULL PLEASE FIX";
+        try {
+            String line = reader.readLine();
+            while (line != null)
+            {
+                if(line.charAt(0) == '['){
+                    //Is it our line?
+                    if(line.contains(tag)){
+                        //Take the second half
+                        String stringList = line.split("]")[1];
+                        String[] strings = stringList.split(",");
+
+                        //Generate random one instead.
+                        int level = ThreadLocalRandom.current().nextInt(0, strings.length + 1);
+                        returnThis = strings[level].trim();
                     }
                 }
                 line = reader.readLine();
